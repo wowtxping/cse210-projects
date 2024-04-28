abb634a4d7c9081e08f79bd4f50406fa2129ef4d
wowtxping <wowtxping@gmail.com>
2024-04-28 10:38:08 +0700

    Prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGerator = new Random();
+        int magicNumber = randomGerator.Next(1,101);
+        Console.Write("What's your guess? ");
+        int guessNumber = int.Parse(Console.ReadLine());
+        while (guessNumber != magicNumber)
+        {
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What's your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What's your guess? ");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("you guessed it!");
     }
 }
