1fd003a24a6e42f3ae222026dfec2bf4c69ff32e
wowtxping <wowtxping@gmail.com>
2024-04-28 08:43:28 +0700

    Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What's your first name? ");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("What's your last name? ");
+        string last_name = Console.ReadLine();        
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!!!");
-        Console.Write("Hello world again!");
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is: {color}");
     }
 }
