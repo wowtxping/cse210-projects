bec5c3adfb24288d42f3261a0fb2da10c96e39c9
wowtxping <wowtxping@gmail.com>
2024-04-28 08:56:11 +0700

    Prep1 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What's your first name? ");
+        Console.Write("What's your first name? ");
         string first_name = Console.ReadLine();
-        Console.WriteLine("What's your last name? ");
+        Console.Write("What's your last name? ");
         string last_name = Console.ReadLine();        
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
