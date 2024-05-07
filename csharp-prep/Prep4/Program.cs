51b87ddcf9402b61cbc60fe50b65e675cedaa5c0
wowtxping <wowtxping@gmail.com>
2024-05-08 01:31:12 +0700

    Prep4 completed.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        while (number !=0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        int sum = 0;
+        int max = 0;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float average = (float)sum/numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max is: {max}");
     }
 }
