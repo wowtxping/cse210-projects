7006f5b162ef3e4f1e98cf94776464daf569e098
wowtxping <wowtxping@gmail.com>
2024-05-08 22:05:07 +0700

    Prep5 completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number? ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int Squared = number * number;
+            return Squared;
+        }
+
+        static void DisplayResult(string name, int SquaredNumber)
+        {
+            Console.WriteLine($"{name}, the squre of your number is {SquaredNumber}");
+        }
+
+        static void main()
+        {
+            DisplayWelcome();
+            string name = PromptUserName();
+            int number = PromptUserNumber();
+            int squred = SquareNumber(number);
+            DisplayResult(name, squred);
+        }
+
+        main();
     }
 }
